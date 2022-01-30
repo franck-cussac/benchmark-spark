d99770190197a1acdf00df964e1e00fc9b8f2f99
scauglog <4425250+scauglog@users.noreply.github.com>
2022-01-30 13:09:21 +0100

    add schema

--- benchmark/src/main/scala/ked/Benchmark.scala
+++ benchmark/src/main/scala/ked/Benchmark.scala
@@ -1,25 +1,52 @@
 package ked
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object Benchmark {
     val APP_NAME = "spark-benchmark"
-    def main(args: Array[String]): Unit = {
-        if (args.size != 3) {
-            println("this app take 3 arguments; experimentName, queryMode, inputPath, outputPath")
-            return
+
+    implicit class OptionOps[A](opt: Option[A]) {
+        def toTry(msg: String): Try[A] = {
+        opt
+            .map(Success(_))
+            .getOrElse(Failure(new NoSuchElementException(msg)))
         }
+    }
 
-        val experimentName = args(0)
-        val queryMode = args(1)
-        val inputPath = args(2)
-        val outputPath = args(3)
-
+    def main(args: Array[String]): Unit = {
         implicit val spark = SparkSession.builder()
             .appName(APP_NAME)
             .getOrCreate()
         import spark.implicits._
 
+        println(s"sys.env = ${sys.env}")
+
+        val res = for {
+            region <- sys.env.get("REGION").toTry("region parameter not found")
+            access_key <- sys.env.get("SCW_ACCESS_KEY").toTry("access_key parameter not found")
+            secret_key <- sys.env.get("SCW_SECRET_KEY").toTry("secret_key parameter not found")
+            inputPath <- sys.env.get("INPUT").toTry("input parameter not found")
+            outputPath <- sys.env.get("OUTPUT").toTry("output parameter not found")
+            experimentName <- sys.env.get("EXPERIMENT_NAME").toTry("experiment_name parameter not found")
+            queryMode <- sys.env.get("QUERY_MODE").toTry("query_mode parameter not found")
+        } yield {
+            spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", s"https://s3.$region.scw.cloud");
+            spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", access_key)
+            spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secret_key)
+            spark.sparkContext.hadoopConfiguration.set("mapreduce.fileoutputcommitter.algorithm.version", "2")
+            start(experimentName, queryMode, inputPath, outputPath)
+        }
+
+        if (res.isFailure) {
+            throw res.failed.get
+        }
+        else {
+            println("Run with success !")
+        }
+        spark.stop()
+    }
+
+    def start(experimentName: String, queryMode: String, inputPath: String, outputPath: String)(implicit spark: SparkSession): Unit = {
         Tables.createAll(inputPath)
         val queries = queryMode match {
             case "simple" => Queries.simple
@@ -30,11 +57,11 @@ object Benchmark {
         val benchmarkResults = queries.map { case (name, query) => 
             println(s"execute query $name")
             val startTime = System.currentTimeMillis()
-            spark.sql(query).write.mode("overwrite").parquet(s"$outputPath/query_results/$name")
+            spark.sql(query).write.mode(SaveMode.Overwrite).parquet(s"$outputPath/query_results/$name")
             val endTime = System.currentTimeMillis()
             (name, endTime - startTime)
         }
 
-        benchmarkResults.toDF("query_name", "execution_time").coalesce(1).write.mode("overwrite").csv(s"$outputPath/experiment/name=$experimentName")
+        benchmarkResults.toDF("query_name", "execution_time").coalesce(1).write.mode(SaveMode.Overwrite).csv(s"$outputPath/experiment/name=$experimentName")
   }
 }
